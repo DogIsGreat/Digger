ec91d035b3e384f6c326a8e58bb0a96add1c825e
Anonymous <email@example.com>
2024-02-14 15:56:20 -0500

    Sql basic setup started.

--- src/odbcdrivetest.c
+++ src/odbcdrivetest.c
@@ -7,5 +7,27 @@
 int main()
 {
     printf(" ODBC Libraries Linked. ");
+    SQLHENV env;
+    SQLHDBC dbc;
+    SQLHSTMT stmt;
+    SQLRETURN ret; /* ODBC API return status */
+    char name[NAME_LENGTH];
+    char *names[MAX_NAMES];
+    int name_count = 0;
+
+    // Allocate an environment handle
+    SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &env);
+
+    // Set the ODBC version to 3.0
+    SQLSetEnvAttr(env, SQL_ATTR_ODBC_VERSION, (void *)SQL_OV_ODBC3, 0);
+
+    // Allocate a connection handle
+    SQLAllocHandle(SQL_HANDLE_DBC, env, &dbc);
+
+    // Connect to the database
+    SQLConnect(dbc, (SQLCHAR *) "DSN=YourDSNName", SQL_INTS, (SQLCHAR *)"user", SQL_NTS, (SQLCHAR *)"password", SQL_NTS);
+
+
+
     return 0;
 }
